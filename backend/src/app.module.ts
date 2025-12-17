02d4cde9253d80992b0fc9b132cd1f47714b2410
wxh <xuhui.wu@goodwe.com>
2025-12-17 14:00:54 +0800

    fix(backend): add ConfigModule to load .env in production

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WallpapersModule } from './wallpapers/wallpapers.module';
 
 @Module({
-  imports: [WallpapersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    WallpapersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
